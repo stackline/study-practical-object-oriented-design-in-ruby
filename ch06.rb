41ed291664a1f9633eed0bac6c7d36f6ea733bed
stackline <kyo10itimanben+noreply@gmail.com>
2018-06-15 17:39:26 +0900

    chapter 6.2

--- ch06.rb
+++ ch06.rb
@@ -5,25 +5,35 @@
 
 # Road bike class
 class Bicycle
-  attr_reader :size, :tape_color
+  attr_reader :style, :size, :tape_color,
+              :front_shock, :rear_shock
 
   def initialize(args)
+    @style = args[:style]
     @size = args[:size]
     @tape_color = args[:tape_color]
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
   end
 
   def spares
-    {
-      chain: '10-speed',
-      tire_size: '23',
-      tape_color: tape_color
-    }
+    if style == :road
+      { chain: '10-speed',
+        tire_size: '23', # milimeters
+        tape_color: tape_color }
+    else
+      { chain: '10-speed',
+        tire_size: '2.1', # inches
+        rear_shock: rear_shock }
+    end
   end
 end
 
 bike = Bicycle.new(
-  size: 'M',
-  tape_color: 'red'
+  style: :mountain,
+  size: 'S',
+  front_shock: 'Manitou',
+  rear_shock: 'Fox'
 )
 
 p bike.size
