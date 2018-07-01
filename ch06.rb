de8ddbc0174a42207ab717a1af8374f16546ad34
stackline <kyo10itimanben+noreply@gmail.com>
2018-07-01 17:37:52 +0900

    chapter 6.3

--- ch06.rb
+++ ch06.rb
@@ -4,7 +4,7 @@
 # [x] 6.2
 # [ ] 6.3
 
-# Road bike class
+# Bicycle class
 class Bicycle
   attr_reader :style, :size, :tape_color,
               :front_shock, :rear_shock
@@ -30,13 +30,26 @@ class Bicycle
   end
 end
 
-bike = Bicycle.new(
-  style: :mountain,
+# Mountain bike class
+class MountainBike < Bicycle
+  attr_reader :front_shock, :rear_shock
+
+  def initialize(args)
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
+    super(args)
+  end
+
+  def spares
+    super.merge(rear_shock: rear_shock)
+  end
+end
+
+mountain_bike = MountainBike.new(
   size: 'S',
   front_shock: 'Manitou',
   rear_shock: 'Fox'
 )
 
-p bike.size
-p bike.spares
-
+p mountain_bike.size
+p mountain_bike.spares
