9505c72b1bcf715cb1438999adb52aca7fb8448d
stackline <kyo10itimanben+noreply@gmail.com>
2018-09-02 22:01:13 +0900

    chapter 6.5 Refactor mountain bike class to reduce dependency on super class

--- ch06.rb
+++ ch06.rb
@@ -68,18 +68,17 @@ end
 class MountainBike < Bicycle
   attr_reader :front_shock, :rear_shock
 
-  def initialize(args)
+  def post_initialize(args)
     @front_shock = args[:front_shock]
     @rear_shock = args[:rear_shock]
-    super(args)
-  end
-
-  def spares
-    super.merge(rear_shock: rear_shock)
   end
 
   def default_tire_size
     '2.1'
+  end
+
+  def local_spares
+    { rear_shock: rear_shock }
   end
 end
 
