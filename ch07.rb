c78e88d1a0585ca1193278930bb33b4608a6414f
stackline <kyo10itimanben+noreply@gmail.com>
2018-09-03 02:25:57 +0900

    chapter 7.1 Add classes responsible for preparer

--- ch07.rb
+++ ch07.rb
@@ -1,3 +1,22 @@
 # Share role behavior with module
 
-# [ ] 7.1 p.180
+# [ ] 7.1 p.181
+
+require 'minitest/autorun'
+
+# Preparer is preparable
+class Mechanic
+  def prepare_trip
+    'aaa'
+  end
+end
+class TripCoordinator
+  def prepare_trip
+    'bbb'
+  end
+end
+class Driver
+  def prepare_trip
+    'ccc'
+  end
+end
