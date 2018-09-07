cd23c1e6a8f96341dde0dab93bd0eb9068e6754e
stackline <kyo10itimanben+noreply@gmail.com>
2018-09-07 23:33:22 +0900

    chapter 7.1 Add schedule class interface

--- ch07.rb
+++ ch07.rb
@@ -1,5 +1,15 @@
 # Share role behavior with module
 
-# [ ] 7.1 p.181
+# [ ] 7.1 p.186
 
 require 'minitest/autorun'
+
+# schedule
+class Schedule
+  # interface
+  def scheduled?(target, starting, ending) end
+
+  def add(target, starting, ending) end
+
+  def remove(target, starting, ending) end
+end
