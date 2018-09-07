069e91d481636a3a8a17e3438c30f8f2f6a53587
stackline <kyo10itimanben+noreply@gmail.com>
2018-09-07 22:57:38 +0900

    chapter 7.1 Delete unnecessary classes

--- ch07.rb
+++ ch07.rb
@@ -3,20 +3,3 @@
 # [ ] 7.1 p.181
 
 require 'minitest/autorun'
-
-# Preparer is preparable
-class Mechanic
-  def prepare_trip
-    'aaa'
-  end
-end
-class TripCoordinator
-  def prepare_trip
-    'bbb'
-  end
-end
-class Driver
-  def prepare_trip
-    'ccc'
-  end
-end
