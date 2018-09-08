e1a415a8040e0df17a32837cf6a6a8360380e9ba
stackline <kyo10itimanben+noreply@gmail.com>
2018-09-08 15:05:48 +0900

    chapter 7.1 Add Schedule class and a unit test

--- ch07.rb
+++ ch07.rb
@@ -6,10 +6,27 @@ require 'minitest/autorun'
 
 # schedule
 class Schedule
-  # interface
-  def scheduled?(target, starting, ending) end
+  def scheduled?(schedulable, start_date, end_date)
+    puts "This #{schedulable.class} is not scheduled\n" \
+         " between #{start_date} and #{end_date}"
+    false
+  end
 
+  # interface
   def add(target, starting, ending) end
 
   def remove(target, starting, ending) end
+end
+
+# test
+class TestSchedule < Minitest::Test
+  def setup
+    @schedule = Schedule.new
+    @object = Class.new
+  end
+
+  def test_scheduled?
+    assert_equal(false,
+                 @schedule.scheduled?(@object, '2018-01-01', '2018-01-02'))
+  end
 end
