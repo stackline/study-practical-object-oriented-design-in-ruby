a848111aea538d3e752b604d31dc42e5377aa0cb
stackline <kyo10itimanben+noreply@gmail.com>
2018-09-08 15:38:13 +0900

    chapter 7.1 Add Bicycle class and a unit test

--- ch07.rb
+++ ch07.rb
@@ -18,7 +18,32 @@ class Schedule
   def remove(target, starting, ending) end
 end
 
-# test
+# bicycle
+class Bicycle
+  attr_reader :schedule, :size, :chain, :tire_size
+
+  def initialize(args = {})
+    @schedule = args[:schedule] || Schedule.new
+  end
+
+  def schedulable?(start_date, end_date)
+    !scheduled?(start_date - lead_days, end_date)
+  end
+
+  private
+
+  def scheduled?(start_date, end_date)
+    schedule.scheduled?(self, start_date, end_date)
+  end
+
+  def lead_days
+    1
+  end
+end
+
+require 'date'
+
+# schedule class test
 class TestSchedule < Minitest::Test
   def setup
     @schedule = Schedule.new
@@ -28,5 +53,18 @@ class TestSchedule < Minitest::Test
   def test_scheduled?
     assert_equal(false,
                  @schedule.scheduled?(@object, '2018-01-01', '2018-01-02'))
+  end
+end
+
+# bicycle class test
+class TestBicycle < Minitest::Test
+  def setup
+    @bike = Bicycle.new
+  end
+
+  def test_schedulable?
+    starting = Date.parse('2015/09/04')
+    ending = Date.parse('2015/09/10')
+    assert_equal(true, @bike.schedulable?(starting, ending))
   end
 end
