29b1b6852123e601a6dd13146824bd28c3687fc4
stackline <kyo10itimanben+noreply@gmail.com>
2018-09-13 13:32:19 +0900

    chapter 8.1 Add bicycle class that has a part

--- ch08.rb
+++ ch08.rb
@@ -1,4 +1,27 @@
 # Combine objects by composition
 
 # [x] 8 intro
-# [ ] 8.1 p.206
+# [ ] 8.1 p.208
+
+class Bicycle
+  attr_reader :size, :parts
+
+  def initialize(args = {})
+    @size = args[:size]
+    @parts = args[:parts]
+  end
+
+  def spares
+    parts.spares
+  end
+end
+
+class Parts
+  def spares
+    'spare is hogehoge.'
+  end
+end
+
+parts = Parts.new
+bike = Bicycle.new(size: 'S', parts: parts)
+p bike.spares
