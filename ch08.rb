061dd4517d67787c7b59b68518592bcc149bdb31
stackline <kyo10itimanben+noreply@gmail.com>
2018-09-15 22:50:33 +0900

    chapter 8.1 Port spare logic from Bicycle class to Parts class

--- ch08.rb
+++ ch08.rb
@@ -17,11 +17,69 @@ class Bicycle
 end
 
 class Parts
+  attr_reader :chain, :tire_size
+
+  def initialize(args = {})
+    @chain = args[:chain] || default_chain
+    @tire_size = args[:tire_size] || default_tire_size
+    post_initialize(args)
+  end
+
+  def default_chain
+    '10-speed'
+  end
+
+  def default_tire_size
+    raise NotImplementedError
+  end
+
+  # subclasses may override
+  def post_initialize(_args)
+    nil
+  end
+
   def spares
-    'spare is hogehoge.'
+    {
+      tire_size: tire_size,
+      chain: chain
+    }.merge(local_spares)
+  end
+
+  def local_spares
+    {}
   end
 end
 
-parts = Parts.new
-bike = Bicycle.new(size: 'S', parts: parts)
-p bike.spares
+class RoadBikeParts < Parts
+  attr_reader :tape_color
+
+  def post_initialize(args)
+    @tape_color = args[:tape_color]
+  end
+
+  def default_tire_size
+    '23'
+  end
+
+  def local_spares
+    { tape_color: tape_color }
+  end
+end
+
+class MountainBikeParts < Parts
+  attr_reader :front_shock, :rear_shock
+
+  def post_initialize(args)
+    @front_shock = args[:front_shock]
+    @rear_shock = args[:rear_shock]
+  end
+
+  def default_tire_size
+    '2.1'
+  end
+
+  def local_spares
+    { rear_shock: rear_shock }
+  end
+end
+
