018e98cec6ca7fe0255c9ade53163ed2ad8e586f
stackline <kyo10itimanben+noreply@gmail.com>
2018-09-16 01:22:02 +0900

    chapter 8.2 Add parts class that is composed with many parts

--- ch08.rb
+++ ch08.rb
@@ -2,9 +2,10 @@
 
 # [x] 8 intro
 # [x] 8.1
-# [ ] 8.2 p.211
+# [ ] 8.2 p.216
 
 require 'minitest/autorun'
+# require 'pry-byebug'
 
 class Bicycle
   attr_reader :size, :parts
@@ -20,77 +21,35 @@ class Bicycle
 end
 
 class Parts
-  attr_reader :chain, :tire_size
+  attr_reader :parts
 
-  def initialize(args = {})
-    @chain = args[:chain] || default_chain
-    @tire_size = args[:tire_size] || default_tire_size
-    post_initialize(args)
-  end
-
-  def default_chain
-    '10-speed'
-  end
-
-  def default_tire_size
-    raise NotImplementedError
-  end
-
-  # subclasses may override
-  def post_initialize(_args)
-    nil
+  def initialize(parts)
+    @parts = parts
   end
 
   def spares
-    {
-      tire_size: tire_size,
-      chain: chain
-    }.merge(local_spares)
-  end
-
-  def local_spares
-    {}
+    parts.select(&:needs_spare)
   end
 end
 
-class RoadBikeParts < Parts
-  attr_reader :tape_color
+class Part
+  attr_reader :name, :description, :needs_spare
 
-  def post_initialize(args)
-    @tape_color = args[:tape_color]
-  end
-
-  def default_tire_size
-    '23'
-  end
-
-  def local_spares
-    { tape_color: tape_color }
-  end
-end
-
-class MountainBikeParts < Parts
-  attr_reader :front_shock, :rear_shock
-
-  def post_initialize(args)
-    @front_shock = args[:front_shock]
-    @rear_shock = args[:rear_shock]
-  end
-
-  def default_tire_size
-    '2.1'
-  end
-
-  def local_spares
-    { rear_shock: rear_shock }
+  def initialize(args)
+    @name = args[:name]
+    @description = args[:description]
+    @needs_spare = args.fetch(:needs_spare, true)
   end
 end
 
 class TestRoadBikeParts < Minitest::Test
   def setup
+    @chain = Part.new(name: 'chain', description: '10-speed')
+    @road_tire = Part.new(name: 'tire_size', description: '23')
+    @tape = Part.new(name: 'tape_color', description: 'red')
     @road_bike = Bicycle.new(
       size: 'L',
-      parts: RoadBikeParts.new(tape_color: 'red')
+      parts: Parts.new([@chain, @road_tire, @tape])
     )
   end
 
@@ -101,20 +60,22 @@ class TestRoadBikeParts < Minitest::Test
 
   def test_spares
     puts "\n## The spare parts of the road bike"
-    spares = {
-      tire_size: '23',
-      chain: '10-speed',
-      tape_color: 'red'
-    }
+    spares = [@chain, @road_tire, @tape]
     assert_equal spares, @road_bike.spares
   end
 end
 
 class TestMountainBikeParts < Minitest::Test
   def setup
+    @chain = Part.new(name: 'chain', description: '10-speed')
+    @mountain_tire = Part.new(name: 'tire_size', description: '2.1')
+    @front_shock = Part.new(name: 'front_shock',
+                            description: 'Manitou',
+                            needs_spare: false)
+    @rear_shock = Part.new(name: 'rear_shock', description: 'Fox')
     @mountain_bike = Bicycle.new(
       size: 'L',
-      parts: MountainBikeParts.new(rear_shock: 'Fox')
+      parts: Parts.new([@chain, @mountain_tire, @front_shock, @rear_shock])
     )
   end
 
@@ -125,11 +86,7 @@ class TestMountainBikeParts < Minitest::Test
 
   def test_spares
     puts "\n## The spare parts of the mountain bike"
-    spares = {
-      tire_size: '2.1',
-      chain: '10-speed',
-      rear_shock: 'Fox'
-    }
+    spares = [@chain, @mountain_tire, @rear_shock]
     assert_equal spares, @mountain_bike.spares
   end
 end
