0633084e1e0e2ae38ba329dc121b654d6260b56f
stackline <kyo10itimanben+noreply@gmail.com>
2018-09-17 21:50:05 +0900

    chapter 8.4 Pass parts object created by parts factory class to bicycle class

--- ch08.rb
+++ ch08.rb
@@ -4,12 +4,10 @@
 # [x] 8.1
 # [x] 8.2
 # [x] 8.3
-# [ ] 8.4 p.224
+# [ ] 8.4 p.227
 
 require 'minitest/autorun'
 # require 'pry-byebug'
-require 'forwardable'
-require 'ostruct'
 
 class Bicycle
   attr_reader :size, :parts
@@ -24,6 +22,7 @@ class Bicycle
   end
 end
 
+require 'forwardable'
 class Parts
   extend Forwardable
   def_delegators :@parts, :size, :each
@@ -38,16 +37,7 @@ class Parts
   end
 end
 
-class Part
-  attr_reader :name, :description, :needs_spare
-
-  def initialize(args)
-    @name = args[:name]
-    @description = args[:description]
-    @needs_spare = args.fetch(:needs_spare, true)
-  end
-end
-
+require 'ostruct'
 module PartsFactory
   class << self
     def build(config, parts_class = Parts)
@@ -67,109 +57,90 @@ module PartsFactory
   end
 end
 
-class TestRoadBikeParts < Minitest::Test
+class TestBicycle < Minitest::Test
   def setup
-    @chain = Part.new(name: 'chain', description: '10-speed')
-    @road_tire = Part.new(name: 'tire_size', description: '23')
-    @tape = Part.new(name: 'tape_color', description: 'red')
-    @road_bike = Bicycle.new(
-      size: 'L',
-      parts: Parts.new([@chain, @road_tire, @tape])
-    )
+    @parts = PartsFactory.build(road_config)
+    @road_bike = Bicycle.new(size: 'L', parts: @parts)
   end
 
   def test_size
-    puts "\n## The size of the road bike"
+    puts "\nBicycle#size"
     assert_equal 'L', @road_bike.size
   end
 
-  def test_spares
-    puts "\n## The spare parts of the road bike"
-    spares = [@chain, @road_tire, @tape]
-    assert_equal spares, @road_bike.spares
-  end
-end
-
-class TestMountainBikeParts < Minitest::Test
-  def setup
-    @chain = Part.new(name: 'chain', description: '10-speed')
-    @mountain_tire = Part.new(name: 'tire_size', description: '2.1')
-    @front_shock = Part.new(name: 'front_shock',
-                            description: 'Manitou',
-                            needs_spare: false)
-    @rear_shock = Part.new(name: 'rear_shock', description: 'Fox')
-    @mountain_bike = Bicycle.new(
-      size: 'L',
-      parts: Parts.new([@chain, @mountain_tire, @front_shock, @rear_shock])
-    )
-  end
-
-  def test_size
-    puts "\n## The size of the mountain bike"
-    assert_equal 'L', @mountain_bike.size
+  def test_parts
+    puts "\nBicycle#parts"
+    assert_equal Parts, @road_bike.parts.class
+    assert_equal @parts, @road_bike.parts
   end
 
   def test_spares
-    puts "\n## The spare parts of the mountain bike"
-    spares = [@chain, @mountain_tire, @rear_shock]
-    assert_equal spares, @mountain_bike.spares
-  end
-end
-
-class TestParts < Minitest::Test
-  def setup
-    @chain = Part.new(name: 'chain', description: '10-speed')
-    @mountain_tire = Part.new(name: 'tire_size', description: '2.1')
-    @front_shock = Part.new(name: 'front_shock',
-                            description: 'Manitou',
-                            needs_spare: false)
-    @rear_shock = Part.new(name: 'rear_shock', description: 'Fox')
-    parts = Parts.new([@chain, @mountain_tire, @front_shock, @rear_shock])
-    @mountain_bike = Bicycle.new(size: 'L', parts: parts)
+    puts "\nBicycle#spares"
+    assert_equal Array, @road_bike.spares.class
+    assert_equal @parts.first, @road_bike.spares.first
   end
 
-  def test_spares_size
-    puts "\n## The size of spares that the mountain bike needs"
-    assert_equal 3, @mountain_bike.spares.size
-  end
+  private
 
-  def test_parts_size
-    puts "\n## The size of parts that the mountain bike has"
-    assert_equal 4, @mountain_bike.parts.size
-  end
-
-  def test_plus
-    puts "\n## Parts class does not have a plus method"
-    # MEMO: You can also describe as the following.
-    #
-    # proc = proc { @mountain_bike.parts + @mountain_bike.parts }
-    # assert_raises NoMethodError, &proc
-    assert_raises NoMethodError do
-      @mountain_bike.parts + @mountain_bike.parts
-    end
+  def road_config
+    [
+      %w[chain 10-speed],
+      %w[tire_size 23],
+      %w[tape_color red]
+    ]
   end
 end
 
 class TestPartsFactory < Minitest::Test
   def setup
     @road_parts = PartsFactory.build(road_config)
+  end
+
+  def test_build
+    puts "\nPartsFactory.build"
+    assert_equal Parts, @road_parts.class
+    assert_equal 3, @road_parts.size
+    part = OpenStruct.new(
+      name: 'chain',
+      description: '10-speed',
+      needs_spare: true
+    )
+    assert_equal part, @road_parts.spares.first
+  end
+
+  private
+
+  def road_config
+    [
+      %w[chain 10-speed],
+      %w[tire_size 23],
+      %w[tape_color red]
+    ]
+  end
+end
+
+class TestParts < Minitest::Test
+  def setup
+    @road_parts = PartsFactory.build(road_config)
     @mountain_parts = PartsFactory.build(mountain_config)
   end
 
-  def test_build_road_parts
-    puts "\n## Build road parts class from road config"
-    assert_equal Parts, @road_parts.class
-    assert_equal OpenStruct, @road_parts.spares.first.class
+  def test_size
+    puts "\nParts#size"
     assert_equal 3, @road_parts.size
-    assert_equal 3, @road_parts.spares.size
+    assert_equal 4, @mountain_parts.size
   end
 
-  def test_build_mountain_parts
-    puts "\n## Build mountain parts class from mountain config"
-    assert_equal Parts, @mountain_parts.class
-    assert_equal OpenStruct, @mountain_parts.spares.first.class
-    assert_equal 4, @mountain_parts.size
+  def test_spares
+    puts "\nParts#spares"
+    assert_equal 3, @road_parts.spares.size
     assert_equal 3, @mountain_parts.spares.size
+    part = OpenStruct.new(
+      name: 'chain',
+      description: '10-speed',
+      needs_spare: true
+    )
+    assert_equal part, @road_parts.spares.first
   end
 
   private
