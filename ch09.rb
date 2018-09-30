4d9c52c85c2a9c512c48d9d11000a2ce09bb48a5
stackline <kyo10itimanben+noreply@gmail.com>
2018-09-30 21:45:23 +0900

    chapter 9.2 Add Gear class unit test

--- ch09.rb
+++ ch09.rb
@@ -43,3 +43,10 @@ class WheelTest < MiniTest::Test
     assert_in_delta(29, wheel.diameter, 0.01)
   end
 end
+
+class GearTest < MiniTest::Test
+  def test_calculates_gear_inches
+    gear = Gear.new(chainring: 52, cog: 11, rim: 26, tire: 1.5)
+    assert_in_delta(137.1, gear.gear_inches, 0.01)
+  end
+end
