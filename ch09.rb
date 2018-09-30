b906d84bdd4600ad3095774b91253bf31abd7c76
stackline <kyo10itimanben+noreply@gmail.com>
2018-09-30 23:57:23 +0900

    chapter 9.2 Show dependency by injecting a wheel object into a gear object

--- ch09.rb
+++ ch09.rb
@@ -19,17 +19,16 @@ class Wheel
 end
 
 class Gear
-  attr_reader :chainring, :cog, :rim, :tire
+  attr_reader :chainring, :cog, :wheel
 
   def initialize(args)
     @chainring = args[:chainring]
     @cog = args[:cog]
-    @rim = args[:rim]
-    @tire = args[:tire]
+    @wheel = args[:wheel]
   end
 
   def gear_inches
-    ratio * Wheel.new(rim, tire).diameter
+    ratio * wheel.diameter
   end
 
   def ratio
@@ -46,7 +45,8 @@ end
 
 class GearTest < MiniTest::Test
   def test_calculates_gear_inches
-    gear = Gear.new(chainring: 52, cog: 11, rim: 26, tire: 1.5)
+    wheel = Wheel.new(26, 1.5)
+    gear = Gear.new(chainring: 52, cog: 11, wheel: wheel)
     assert_in_delta(137.1, gear.gear_inches, 0.01)
   end
 end
