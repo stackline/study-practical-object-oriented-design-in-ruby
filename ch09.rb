d32d232d241d9f5cfdddd71148d491c6e37aba7c
stackline <kyo10itimanben+noreply@gmail.com>
2018-09-30 18:35:04 +0900

    chapter 9.2 Add test target code

--- ch09.rb
+++ ch09.rb
@@ -2,3 +2,35 @@
 
 # [x] 9.1
 # [ ] 9.2 p.249
+
+class Wheel
+  attr_reader :rim, :tire
+
+  def initialize(rim, tire)
+    @rim = rim
+    @tire = tire
+  end
+
+  def diameter
+    rim + (tire * 2)
+  end
+end
+
+class Gear
+  attr_reader :chainring, :cog, :rim, :tire
+
+  def initialize(args)
+    @chainring = args[:chainring]
+    @cog = args[:cog]
+    @rim = args[:rim]
+    @tire = args[:tire]
+  end
+
+  def gear_inches
+    ratio * Wheel.new(rim, tire).diameter
+  end
+
+  def ratio
+    chainring / cog.to_f
+  end
+end
