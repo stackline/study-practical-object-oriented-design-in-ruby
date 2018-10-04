bb13b5f45ded544cc5b0cbaed8c37dcc45f009ce
stackline <kyo10itimanben+noreply@gmail.com>
2018-10-04 19:41:38 +0900

    chapter 9.6 Ensure that parent class and inherited child class satisfy Liskov's substitution principle

--- ch09.rb
+++ ch09.rb
@@ -5,7 +5,7 @@
 # [x] 9.3
 # [x] 9.4
 # [x] 9.5
-# [ ] 9.6 p.281
+# [ ] 9.6 p.285
 
 class Wheel
   attr_reader :rim, :tire
@@ -104,5 +104,56 @@ class Trip
     preparers.each do |preparer|
       preparer.prepare_trip(self)
     end
+  end
+end
+
+class Bicycle
+  attr_reader :size, :chain, :tire_size
+
+  def initialize(args = {})
+    @size = args[:size]
+    @chain = args[:chain] || default_chain
+    @tire_size = args[:tire_size] || default_tire_size
+    post_initialize(args)
+  end
+
+  def spares
+    {
+      tire_size: tire_size,
+      chain: chain
+    }.merge(local_spares)
+  end
+
+  def default_tire_size
+    raise NotImplementedError
+  end
+
+  # subclasses may override
+  def post_initialize(_args)
+    nil
+  end
+
+  def local_spares
+    {}
+  end
+
+  def default_chain
+    '10-speed'
+  end
+end
+
+class RoadBike < Bicycle
+  attr_reader :tape_color
+
+  def post_initialize(args)
+    @tape_color = args[:tape_color]
+  end
+
+  def local_spares
+    { tape_color: tape_color }
+  end
+
+  def default_tire_size
+    '23'
   end
 end
--- ch09_test.rb
+++ ch09_test.rb
@@ -103,3 +103,45 @@ class TripTest < MiniTest::Test
     assert_equal true, preparer.verify
   end
 end
+
+module BicycleInterfaceTest
+  def test_responds_to_size
+    assert_respond_to(@object, :size)
+  end
+
+  def test_responds_to_chain
+    assert_respond_to(@object, :chain)
+  end
+
+  def test_responds_to_tire_size
+    assert_respond_to(@object, :tire_size)
+  end
+
+  def test_responds_to_spares
+    assert_respond_to(@object, :spares)
+  end
+
+  def test_responds_to_default_tire_size
+    assert_respond_to(@object, :default_tire_size)
+  end
+
+  def test_responds_to_default_chain
+    assert_respond_to(@object, :default_chain)
+  end
+end
+
+class BicycleTest < MiniTest::Test
+  include BicycleInterfaceTest
+
+  def setup
+    @bike = @object = Bicycle.new(tire_size: 0)
+  end
+end
+
+class RoadBikeTest < MiniTest::Test
+  include BicycleInterfaceTest
+
+  def setup
+    @bike = @object = RoadBike.new
+  end
+end
