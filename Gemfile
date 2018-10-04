49a3f1f39e832c4f4362414f7963691f8c98951f
stackline <kyo10itimanben+noreply@gmail.com>
2018-10-04 16:24:15 +0900

    chapter 9.5 Use Minitest-reporter to format test results

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
 group :development do
   gem 'minitest'
+  gem 'minitest-reporters'
   gem 'pry-byebug'
   gem 'simplecov'
 end
--- ch09_test.rb
+++ ch09_test.rb
@@ -2,6 +2,9 @@ require 'simplecov'
 SimpleCov.start
 
 require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
 require_relative 'ch09'
 
 class WheelTest < MiniTest::Test
